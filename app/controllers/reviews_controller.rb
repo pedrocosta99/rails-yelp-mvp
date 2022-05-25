059764d9db6a6599268a8348f31699aac0c4b076
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-05-25 14:48:36 -0300

    rest n review model controller, almost no view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,26 @@
 class ReviewsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
-  end
-
-  def edit
+    # since it is a children, it needs its parent ID !!!!!!
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+    @review.save
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def update
-  end
+  private
 
-  def destroy
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
